44e1346407a7d24075ae71c455767af03c1646c8
chetanuchiha16 <chetan16ck@gmail.com>
2026-04-22 21:35:31 +0530

    Implement get_image_paths function to retrieve JPG and PNG image paths

--- src/get_image_bytes.rs
+++ src/get_image_bytes.rs
@@ -1,1 +1,21 @@
+use std::{
+    fs,
+    io::{Error},
+    path::PathBuf,
+};
+
+pub fn get_image_paths(image_path: &str) -> Result<Vec<PathBuf>, Error> {
+    let image_dir = fs::read_dir(image_path)?;
+    let image_paths = image_dir
+        .into_iter()
+        .filter_map(|p| p.ok())
+        .map(|x| x.path())
+        .filter(|x| {
+            x.extension().and_then(|x| x.to_str()) == Some("jpg")
+                || x.extension().and_then(|x| x.to_str()) == Some("png")
+        })
+        .collect();
+    Ok(image_paths)
+}
+
 pub fn get_encoded_image_bytes() {}
