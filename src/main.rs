a8b693aff7cd080829eee23a5f09ad8696056ca3
chetanuchiha16 <chetan16ck@gmail.com>
2026-04-23 20:21:34 +0530

    Refactor main function to correct image path and enhance logging

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,13 @@ use crate::{
 mod get_image_bytes;
 mod image_processors;
 fn main() -> Result<(), Box<dyn std::error::Error>>{
-    let image_paths = get_image_paths("./images")?;
+    let cwd = std::env::current_dir()?;
+    println!("The current directory is: {:?}", cwd);
+    let image_paths = get_image_paths("./src/images")?;
     let encoded_image_bytes = get_encoded_image_bytes(&image_paths)?;
     process_single_image(&encoded_image_bytes[0])?;
     process_multiple_images(&encoded_image_bytes)?;
     parallel_process_images(&encoded_image_bytes)?;
-    println!("Hello, world!");
+    println!("processed {} images", image_paths.len());
     Ok(())
 }
