5dc1b8b8ead555b1bcf47f16fa63b89ea2dbcb96
chetanuchiha16 <chetan16ck@gmail.com>
2026-04-25 20:24:51 +0530

    Save processed single image to output directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::Cursor;
+
 use crate::{
     get_image_bytes::{get_encoded_image_bytes, get_image_paths},
     image_processors::{parallel_process_images, process_multiple_images, process_single_image},
@@ -23,7 +25,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
     let image_paths = get_image_paths(&args.path)?;
     let encoded_image_bytes = get_encoded_image_bytes(&image_paths)?;
-    process_single_image(&encoded_image_bytes[0])?;
+    let single_processed_image =  process_single_image(&encoded_image_bytes[0])?;
+    image::ImageReader::new(Cursor::new(single_processed_image)).with_guessed_format()?.decode()?.save("src/images/op/single_image.jpg")?;
     process_multiple_images(&encoded_image_bytes)?;
     parallel_process_images(&encoded_image_bytes)?;
     println!("processed {} images", image_paths.len());
