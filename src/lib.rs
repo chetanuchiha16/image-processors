cd79f1238b3971ab4cceb6eeee8991cfd7f25b01
chetanuchiha16 <chetan16ck@gmail.com>
2026-04-30 07:56:04 +0530

    Implement error handling and add py_process_images function for batch image processing

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,27 @@
 use pyo3::prelude::*;
 
+use crate::image_processors::ProcessorError;
+pub mod get_image_bytes;
+pub mod image_processors;
+
+impl From<ProcessorError> for PyErr {
+    fn from(err: ProcessorError) -> PyErr {
+        pyo3::exceptions::PyValueError::new_err(err.to_string())
+    }
+}
 /// A Python module implemented in Rust.
 #[pymodule]
 mod file {
     use pyo3::prelude::*;
+
+    use crate::image_processors::{process_multiple_images};
+
+    #[pyfunction]
+    fn py_process_images(encoded_image_bytes: Vec<Vec<u8>>) -> PyResult<Vec<Vec<u8>>> {
+        let x = process_multiple_images(&encoded_image_bytes)?;
+        Ok(x)
+
+    }
 
     /// Formats the sum of two numbers as string.
     #[pyfunction]
