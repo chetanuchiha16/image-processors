9f19030b2920ee4182ae778e95b28c0cde32949a
chetanuchiha16 <chetan16ck@gmail.com>
2026-05-04 08:23:35 +0530

    Refactor get_image_paths function to improve readability and maintainability

--- src/get_image_bytes.rs
+++ src/get_image_bytes.rs
@@ -1,17 +1,29 @@
 use std::{fs, io::Error, path::PathBuf};
 
 pub fn get_image_paths(image_path: &str) -> Result<Vec<PathBuf>, Error> {
-    let image_dir = fs::read_dir(image_path)?;
-    let image_paths = image_dir
-        .into_iter()
-        .filter_map(|p| p.ok())
-        .map(|x| x.path())
-        .filter(|x| {
-            let extension = x.extension().and_then(|x| x.to_str());
-            extension == Some("jpg") || extension == Some("png")
+    // let image_dir = fs::read_dir(image_path)?;
+    // let image_paths =
+    fs::read_dir(image_path)?
+        // .into_iter()
+        // .filter_map(|p| p.ok())
+        .map(|res| {
+            // let x = x?;
+            res.map(|e: fs::DirEntry| e.path())
+            // Ok(x.path())
         })
-        .collect();
-    Ok(image_paths)
+        .filter(|res| {
+            match res {
+                Ok(path) => {
+                    let ext = path.extension().and_then(|x| x.to_str());
+                    // ext == Some("jpg") || ext == Some("png")
+                    matches!(ext, Some("jpg") | Some("png"))
+                }
+                Err(_) => true,
+            }
+            // let extension = x.extension().and_then(|x| x.to_str());
+        })
+        .collect()
+    // Ok(image_paths)
 }
 
 pub fn get_encoded_image_bytes(paths: &[PathBuf]) -> Result<Vec<Vec<u8>>, Error> {
